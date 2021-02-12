2f0d8208ddb16f71670e7ba6b20ad0b002616528
BetulCengiz <bc199911@hotmail.com>
2021-02-13 02:40:36 +0300

    Add project files.

---
+++ CharDegiskeni/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CharDegiskeni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // char değişkeni tek karakterlik veri tutar ve büyük küçük harf duyarlılığı vardır
+
+
+            //char cinsiyet;
+            //Console.WriteLine("Cinsiyeti girin(e/k): ");
+            //cinsiyet = Convert.ToChar(Console.ReadLine());
+
+            //if (cinsiyet == 'e')
+            //{
+            //    Console.WriteLine("Cinsiyet erkek");
+            //}
+            //else if (cinsiyet== 'k')
+            //{
+                      
+            //    Console.WriteLine("Cinsiyet kadın");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yanlış karakter girişi");
+            //}
+
+            //char harf;
+            //harf = 'b';// tek tırnak kullanılır
+            //Console.WriteLine(harf);
+
+            //---------- &&>>ve------- ||>> veya demek ----------\n bir satır boşluk bırakır---------
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
